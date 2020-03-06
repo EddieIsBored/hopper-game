e2dee2ba1d27c315a2e6bbd1b7feec6090fec4b9
EddieIsBored <e.nokes@lancaster.ac.uk>
2020-03-06 17:57:30 +0000

    5x5 grid set up

--- GUI.java
+++ GUI.java
@@ -12,5 +12,16 @@ public class GUI
         frame.setVisible(true);
         frame.setSize(750,750);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton[][] arr = new JButton[5][5];
+
+        for (int i = 0; i < 5; i++) { 
+            for (int j = 0; j < 5; j++) { 
+                arr[i][j] = new JButton(" R" + i + " C" + j);
+                panel.add(arr[i][j]);
+            }
+        }
+
+        frame.add(panel, BorderLayout.CENTER);
     }
 }
--- Main.java
+++ Main.java
@@ -8,7 +8,8 @@ public class Main
      * Entrypoint - Begins running the game
      * @param args arguments passed to program
      */
-    public static void main(final String[] args){
+    public static void main(final String[] args)
+    {
         final GUI game = new GUI();
     }
 }
